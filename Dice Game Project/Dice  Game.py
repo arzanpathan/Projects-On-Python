72ea699004c250473983743cf952576a929eb8da
Arzan Arif Pathan <arzan.pathan8@gmail.com>
2018-10-09 04:21:41 +0530

    Update Dice  Game.py

--- Dice Game Project/Dice  Game.py
+++ Dice Game Project/Dice  Game.py
@@ -1,7 +1,7 @@
 
 import random
 import time
-import Dice
+import Dice #For Getting The Pattern Of Dice
 def sleep(s):
  for k in range(3):
      print('.',end=' ')
@@ -14,28 +14,38 @@ class Dice_Game:
  print('\n','\n''Two Dice are Rolled','\n')
  def Dice (self,Chance_Count):
      while(Chance_Count<5):
+
          Dice1,Dice2=random.randint(1, 6),random.randint(1, 6)
+
+#Sum Of The Two Dice To Be Entered By User
          try_again=0
          while try_again==0:
              try:
-                 Dice_Num_User=int(input('GUESS THE SUM OF TWO DICE: '))
+                 Dice_Num_User=int(input('GUESS THE SUM OF TWO DICE '))
                  if (Dice_Num_User<2 or Dice_Num_User>12):
                      raise ValueError
                  else:
                      try_again=1
              except ValueError:
                  Chance_Count,Chance_Rem=Chance_Count+1,4-Chance_Count
-                 print('Please Enter The Sum in Range 2 to 12 in Integer Form','\n','\n','You Lose Your Chance','\n','\n','COMPUTER WON','You Have Only',Chance_Rem,'Chances Remaining')
-         
-         if(Dice1+Dice2==Dice_Num_User):
+                 print('Please Enter The Sum in Range 2 to 12 in Integer Form','\n','\n','You Lose Your Chance','\n','\t','COMPUTER WON','\n','\n','You Have Only',Chance_Rem,'Chances Remaining')
+             if Chance_Count==5:
+                 break
+
+
+#To Compare User Values with Computer and Declare Who Won                
+         if Chance_Count==5:
+             break
+         elif(Dice1+Dice2==Dice_Num_User):
              print('\n','I Guess Winning Is Not in Your Destiny LOL ',end=' ')
              sleep(3)
-             print(Dice.Dice_Value(Dice1),'\n','\n','\t',Dice.add(),Dice.Dice_Value(Dice2),Dice1+Dice2,'\n','YOU WON ','\n','Beginner\'s Luck','\n','\n')
+             Dice.Dice_Value(Dice1),Dice.add(),Dice.Dice_Value(Dice2)
+             print('Sum of Two Dice Rolls too: ',Dice1+Dice2,'\n','\n','\t','>>> YOU WON <<<','\n','\n','Beginner\'s Luck','\n','\n')
              try_again=0
              while try_again==0:
                  try:
                      play_again=input('Do You Wanna Play Again(Y/N): ').upper()
-                     if play_again!='Y' and 'N' and 'YES' and 'NO':
+                     if play_again!='Y' and play_again!='N' and play_again!='YES' and play_again!='NO':
                         raise NameError
                      else:
                          try_again=1
@@ -52,22 +62,23 @@ class Dice_Game:
                  
          else:
              Chance_Count,Chance_Rem=Chance_Count+1,4-Chance_Count
-             print('\n','I Guess Winning Is Not in Your Destiny LOL ','\n','\n',end=' ')
+             print('I Guess Winning Is Not in Your Destiny LOL ',end=' ')
              sleep(3)
-             print(Dice.Dice_Value(Dice1),'\n','\t',Dice.add(),Dice.Dice_Value(Dice2))
-             print('Sum of Two Dice Rolls too: ',Dice1+Dice2,'\n','COMPUTER WON','\n','You Have Only',Chance_Rem,'Chances Remaining To Prove Yourself Kiddo')
+             Dice.Dice_Value(Dice1),Dice.add(),Dice.Dice_Value(Dice2)
+             print('Sum of Two Dice Rolls too: ',Dice1+Dice2,'\n','\n','\t','>>> COMPUTER WON <<<','\n','\n','You Have Only',Chance_Rem,'Chances Remaining To Prove Yourself Kiddo','\n','\n')
  
 
-
-            
-
+      
+#To Start The Game
 Guess_Game=Dice_Game()
 Guess_Game.Dice(1)
+
+#Play again block
 try_again=0
 while try_again==0:
     try:
         play_again=input('Do You Wanna Play Again(Y/N): ').upper()
-        if play_again!='Y' and 'N' and 'YES' and 'NO':
+        if play_again!='Y' and play_again!='N' and play_again!='YES' and play_again!='NO':
             raise NameError
         else:
             try_again=1
